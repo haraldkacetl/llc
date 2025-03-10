4ea1f8a81c2b6678e33772fbb74179de6080ee1e
matz <Matzels@wunderland.oo>
2025-03-10 09:08:57 +0100

    New Jail Installer

--- llc_new/install.sh
+++ llc_new/install.sh
@@ -12,7 +12,24 @@ fi
 #apt-get update
 #apt-get install -y apache2
 #apt install jq
+#a2enmod cgi
+#systemctl restart apache2
 
-a2enmod cgi
-systemctl restart apache2
+jpath="/jail-bin"
+echo "set path to $jpath"
+mkdir -p $jpath/jail
+echo "created jail directory"
+mkdir -p $jpath/jail/bin
+echo "created jail/bin directory"
+mkdir -p $jpath/jail/lib
+echo "created jail/lib directory"
+mkdir -p $jpath/jail/lib64
+echo "created jail/lib64 directory"
 
+echo "copying files to jail"
+cp -r -p /bin/* $jpath/jail/bin/
+echo "copied /bin/ to jail/bin/"
+cp -r -p /lib/* $jpath/jail/lib/
+echo "copied /lib/ to jail/lib/"
+cp -r -p /lib64/* $jpath/jail/lib64/
+echo "copied /lib64/ to jail/lib64/"
