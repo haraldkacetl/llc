371a36f7f5d4e3b1e83ea75dab8b558ee5e492ca
Tunka <haraldkacetl@gmail.com>
2025-03-17 09:27:42 +0100

    added the varpath to the command

--- llc_new/command_execution.sh
+++ llc_new/command_execution.sh
@@ -2,9 +2,6 @@
 
 chroot_path="/var/jail-bin/jail"
 
-
-#wmi=$(whoami)
-#echo "$wmi"
 echo "Content-type: text/html; charset=UTF-8"
 echo ""
 
@@ -16,24 +13,34 @@ else
 fi
 
 decode_url() {
-    echo -e "$(echo "$1" | sed 's/+/ /g; s/%\(..\)/\\x\1/g' | xargs -0 printf '%b')"
+    # URL-Parameter dekodieren und zusätzlich %2F in / umwandeln
+    echo -e "$(echo "$1" | sed 's/+/ /g; s/%2F/\//g; s/%\(..\)/\\x\1/g' | xargs -0 printf '%b')"
 }
 
 # Extrahiere den eingegebenen Befehl
 input_command=$(echo "$INPUT" | awk -F"&" '{for (i=1; i<=NF; i++) if ($i ~ /^input_command=/) print substr($i,15)}')
-input_command=$(decode_url "$input_command")
+input_command=$(decode_url "$input_command" | tr -d '\r')  # Windows-Zeilenumbrüche entfernen
 
+# Extrahiere den varpath
+varpath=$(echo "$INPUT" | awk -F"&" '{for (i=1; i<=NF; i++) if ($i ~ /^varpath=/) print substr($i,9)}')
+varpath=$(decode_url "$varpath" | tr -d '\r')  # Windows-Zeilenumbrüche entfernen
 
+# Falls varpath leer ist, Standardwert setzen
+if [[ -z "$varpath" ]]; then
+    varpath="/"  # Standard: Root der Jail
+fi
 
-# Führe den Befehl in der Jail-Umgebung aus
+# Führe den Befehl in der Jail-Umgebung aus (Kommando in einer einzigen Shell-Session)
+command_in_jail="cd \"$varpath\" && { $input_command; echo '<path>'; pwd; echo '</path>'; }"
 
-#echo "Benutzer-ID: $(id)"
-input_command=$(echo "$input_command && echo '<path>' && pwd && echo '</path>'")
-command_in_jail="sudo chroot $chroot_path /bin/bash -c \"$input_command\""
-#echo "execution $command_in_jail"
-output=$(eval "$command_in_jail" 2>&1)
+output=$(sudo chroot "$chroot_path" /bin/bash -c "$command_in_jail" 2>&1)
 
 # HTML-Ausgabe generieren
-echo "<html><head><meta charset='UTF-8'></head><body>"
+echo "<!DOCTYPE html>"
+echo "<html><head>"
+echo "<meta charset='UTF-8'>"
+#echo "$varpath"
+#echo "$INPUT"
+echo "<meta charset='UTF-8'></head><body>"
 echo "<pre>$output</pre>"
 echo "</body></html>"
